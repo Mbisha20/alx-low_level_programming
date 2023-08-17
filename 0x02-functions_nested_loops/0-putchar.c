83fc1eb262463bfde4dc887f3a0dcb5afbd344a4
MUAYAD BASHEER <moybash20@gmail.com>
2023-08-16 19:59:26 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "main.h"
-/**
- task one
+/*
+ * main - we use puchar command
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('_');
+        _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
 return (0);
-}
 }
