1bd61f61fe9ee3b59c937543017b5902913d8021
MUAYAD BASHEER <moybash20@gmail.com>
2023-08-21 14:37:09 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+
+void swap_int(int *a, int *b);
+{
+	int k = *a;
+	*a = *b;
+	*b = k;
+	return (0);
+}
