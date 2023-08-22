bba67de2fbc03d24ebcc6dc1e582ba0da88d3d16
MUAYAD BASHEER <moybash20@gmail.com>
2023-08-22 08:44:13 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function to print chars
+ *
+ * returne = 0
+ */
+	void print_alphabet_x10(void)
+{
+	for (int i = 1 ; i <= 10 ; i++)
+	{
+	for (char b = 'a' ; b < = 'z' ; b++)
+	{
+	puchar(b);
+	return (0);
+	}
+	return (0);
+	}
