cb07736953f17af8fc80542c00fde30066f284c5
MUAYAD BASHEER <moybash20@gmail.com>
2023-08-22 09:19:31 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,26 @@
 #include "main.h"
-/**
- * print_alphabet_x10 - function to print chars
- *
- * returne = 0
- */
-	void print_alphabet_x10(void)
-{
-	int i = 0;
 
-	while (i <= 10)
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char g;
+	int m;
+
+	m = 0;
+
+	while (m < 10)
 	{
-	for (char b = 'a' ; b <= 'z' ; b++)
-	{
-	_puchar(b);
-	return (0);
-	}
-	_putchar("\n");
-	i++;
-	return (0);
+		g = 'a';
+		while (g <= 'z')
+		{
+			_putchar(g);
+			g++;
+		}
+		_putchar('\n');
+		i++;
 	}
 }
+
